bdc21a656325b4d56dd7f68e69aecbe38a5a0918
Santiago Sanchez <chagostwo@yahoo.com>
2019-08-09 03:25:27 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,11 +4,11 @@ class App < Sinatra::Base
   get '/name' do
   "My name is who"
   end
-  
+
   get '/' do
   "My home town is where"
   end
-  
+
   get '/favorite-song' do
   "My facorite sons is what"
   end
